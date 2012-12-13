768c3a1ae00fae22c341ce6cc11dec32fc184b36
torianin <tori@robert-i.com>
2012-12-13 19:12:53 +0100

    Try to make beautiful code haml, coffee, sass

--- index.rb
+++ index.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'haml'
+require 'coffee-script'
+
+set :haml, :format => :html5
 
 get '/' do
-  "Hello World"
+  haml :index
+end
+
+get '/app.js' do
+  coffee :app
 end
